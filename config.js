18bba96543c5b17745e215f45be5b1d183e8e70a
cipoeng <hapiis.bg@gmail.com>
2026-06-06 14:12:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: "UserId", //- Replace UserId with the Discord ID of the admin of the bot
+	adminId: "933250501624098816", //- Replace UserId with the Discord ID of the admin of the bot
 	token: process.env.token || "MTEyMTY4MTAyOTY5OTIxMTMwNA.G65GRe.InP1-uuljyGqvlVLU-l24nb631PpNhVLElcXLo", //- Bot's Token
 	clientId: process.env.clientId || "1121681029699211304", //- ID of the bot
 	clientSecret: process.env.clientSecret || "Te0bvZmUdPe4FVrQmXXTW0i7HVOvNZ3H", //- Client Secret of the bot
