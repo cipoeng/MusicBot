6bbb94147ff905f6040d3a4691132994e4b3a94f
cipoeng <hapiis.bg@gmail.com>
2026-06-06 14:22:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
 			identifier: "Main Node", //- Used for indentifier in stats commands.
 			host: "lavalinkv4.serenetia.com", //- The host name or IP of the lavalink server.
 			port: 443, // The port that lavalink is listening to. This must be a number!
-			password: "", //- The password of the lavalink server.
+			password: "https://seretia.link/discord", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: true, //- Can be either true or false. Only use true if ssl is enabled!
